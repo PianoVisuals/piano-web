880a4aadee5bf32cfde7e032839146cb02b35df4
unknown <jeanballetiktok@gmail.com>
2025-07-09 21:20:00 +0200

    fix: base path for Netlify

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/piano-web/",        // ← AJOUTE CETTE LIGNE
+  base: "/",
   plugins: [react()],
 });
