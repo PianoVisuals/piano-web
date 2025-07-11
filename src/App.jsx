dfbba4cd7196052d6d54f7e865169b7f9f9cee7c
unknown <jeanballetiktok@gmail.com>
2025-07-11 17:25:28 +0200

    jsp

--- src/App.jsx
+++ src/App.jsx
@@ -951,7 +951,7 @@ const labelByMidi = useMemo(() => {
   
     /* 2) Piano juste au-dessus de la barre */
     :root[data-mode="rythme"] .piano {
-      bottom: 50px !important; /* ← décale vers le haut */
+      bottom: 2rem !important; /* ← décale vers le haut */
       top: auto !important;
     }
   }
