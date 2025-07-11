f6bf52ec8265bf62c3aa2ba8e0f072debb90e268
unknown <jeanballetiktok@gmail.com>
2025-07-11 16:44:40 +0200

    jsp

--- src/App.jsx
+++ src/App.jsx
@@ -919,16 +919,16 @@ const labelByMidi = useMemo(() => {
   }
 
 
-@media (pointer: coarse) {
-  :root[data-mode="rythme"] {
-    /* on augmente la hauteur relative des touches */
-    --white-h: calc(var(--white-w) * 7); /* au lieu de *4, on passe à *4.5 */
+  @media (pointer: coarse) {
+    :root[data-mode="rythme"] {
+      /* on augmente la hauteur relative des touches */
+      --white-h: calc(var(--white-w) * 7); /* au lieu de *4, on passe à *4.5 */
+    }
+    /* assure que le conteneur prend bien cette nouvelle hauteur */
+    :root[data-mode="rythme"] .piano {
+      height: var(--white-h) !important;
+    }
   }
-  /* assure que le conteneur prend bien cette nouvelle hauteur */
-  :root[data-mode="rythme"] .piano {
-    height: var(--white-h) !important;
-  }
-}
 
 
 
