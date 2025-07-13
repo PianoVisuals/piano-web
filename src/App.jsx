f7c3859fd9acf6ed97fe1ba44516601674592c4b
unknown <jeanballetiktok@gmail.com>
2025-07-13 15:51:51 +0200

    jsp

--- src/App.jsx
+++ src/App.jsx
@@ -1363,7 +1363,7 @@ const labelByMidi = useMemo(() => {
           </div>
         </div>
       </div>
-  </div>
+    </div>
   )}
 
   <div className="piano" ref={pianoRef} onPointerDown={pDown} onPointerMove={pMove} onPointerUp={pUp} onPointerCancel={pUp}>{keys}</div>
