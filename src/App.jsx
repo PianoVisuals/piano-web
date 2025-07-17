fc95846dbf9fb30a0ac1157123d191c31236b735
unknown <jeanballetiktok@gmail.com>
2025-07-17 21:02:22 +0200

    jsp

--- src/App.jsx
+++ src/App.jsx
@@ -1358,7 +1358,7 @@ const labelByMidi = useMemo(() => {
         {privacyText}
       </a>
       <a href={contactHref}>
-        {`${contactText}: ${contactHref.replace('mailto:', '')}`}
+        {contactText}
       </a>
     </div>
 
